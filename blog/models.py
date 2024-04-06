49264ca9333e9627d41af407dac5f20c5265dbfd
vValadia <107342567+vValadia@users.noreply.github.com>
2024-04-06 09:31:53 +0400

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,50 @@ class Post(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
 
+class product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.
+
+
+
+
+
+
+    
+    # id_product = models.CharField(max_length=100000, primary_key=True)
+    # id_category = models.
+    # product_name = models.
+    # price = models.
+    # year = models.
+    # photo = models.
+    # producer = models.
+    # type = models.
+
+class category(models.Model):
+    id_category = models.
+    category = models.
+
+class user(models.Model):
+    id_user = models.
+    id_category = models.
+    user_name = models.
+    login = models.
+    email = models.
+    password = models.
+
+class order(models.Model):
+    id_order = models.
+    id_user = models.
+    date = models.
+    status = models.
+    info = models.
+    
+class order(models.Model):
+    id_order = models.
+    id_product = models.
+    cost = models.
+    quantity = models.
+    
     def publish(self):
         self.published_date = timezone.now()
         self.save()
